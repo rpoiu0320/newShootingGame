ecbc0a8ef18bcaf4e50537a6548e480317490685
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-02 01:52:21 +0900

    feat : target

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField] private ParticleSystem hitEffect;
-    [SerializeField] float maxDistance;
-    [SerializeField] int damage;
+    [SerializeField] private TrailRenderer bulletTrail;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private int damage;
 
-    private TrailRenderer muzzleEffect;
+    private ParticleSystem muzzleEffect;
 
     private void Awake()
     {
-        muzzleEffect = GetComponentInChildren<TrailRenderer>();
+        muzzleEffect = GetComponentInChildren<ParticleSystem>();
     }
 
     public /*virtual*/ void Fire()
     {
+        muzzleEffect.Play();
+
         RaycastHit hit;
         Debug.Log("hit");
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance))
@@ -25,11 +28,15 @@ public class Gun : MonoBehaviour
             ParticleSystem effect = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal)); // LookRotation은 벡터를 넣어주면 그 방향으로 바라보게 해줌, hit.nomal은 충돌 지점에서 충돌체의 면에 수직으로
             effect.transform.parent = hit.transform;
             Destroy(effect.gameObject, 3f);
+
+            TrailRenderer trail = Instantiate(bulletTrail, muzzleEffect.transform.position, Quaternion.identity);
+            //                                  생성할거,       시작 위치,                      회전(identity 회전 없이)
+
             hittable?.Hit(hit, damage);
         }
     }
 
-    /*IEnumerator TrailRoutine(Vector3 startPoint, Vector3 endPoint)
+    IEnumerator TrailRoutine(Vector3 startPoint, Vector3 endPoint)
     {
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance))
@@ -51,5 +58,5 @@ public class Gun : MonoBehaviour
         {
             //TrailRenderer.
         }
-    }*/
+    }
 }
