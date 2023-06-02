9871ae92188fe4052e343b178203c601395d4fca
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-02 15:38:21 +0900

    test

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Target : MonoBehaviour, IHittable
 {
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     public void Hit(RaycastHit hit, int damage)
     {
-
+        if(rb != null)
+        {
+            rb?.AddForceAtPosition(-10 * hit.normal, hit.point, ForceMode.Impulse);
+        }
     }
 }
